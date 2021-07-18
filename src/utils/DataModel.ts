18754704cea1442ca900c39e549153256e45629a
Shaun-Shane <63491699+Shaun-Shane@users.noreply.github.com>
2021-07-18 14:37:15 +0800

    添加 SecondHandDetailModel

--- src/utils/DataModel.ts
+++ src/utils/DataModel.ts
@@ -39,3 +39,27 @@ export interface RentDetailModel {
   rent_type: string;
   equipments: number;
 }
+
+
+export interface SecondHandDetailModel {
+  title: string;
+  photos: string[];
+  house_type: string;
+  area: number;
+  create_time: number;
+  last_modify: number;
+  floor: number;
+  total_floor: number;
+  price: number;
+  owner: {
+    username: string;
+    nickname: string;
+    avatar: string;
+  }
+  decoration: string;
+  features: string[];
+  neighbourhood: string;
+  city: string;
+  rent_type: string;
+  equipments: number;
+}
