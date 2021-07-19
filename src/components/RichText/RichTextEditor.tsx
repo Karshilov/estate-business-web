f20a7016c17bfa02c12fffca2700da1e53c4c8a2
Karshilov <karshilov@foxmail.com>
2021-07-19 20:05:18 +0800

    richText

--- src/components/RichText/RichTextEditor.tsx
+++ src/components/RichText/RichTextEditor.tsx
@@ -1,37 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 
 const RichTextEditor = () => {
-    useEffect(() => {
-        console.log('rich text')
-    }, [])
+  useEffect(() => {
+    console.log('rich text')
+    console.log(ClassicEditor.builtinPlugins.map( (plugin: any) => plugin.pluginName ))
+  }, [])
 
-    return <div style={{ width: '100%', minHeight: '100vh' }}>
-        <CKEditor
-            editor={ClassicEditor}
-            data="<p>Hello from CKEditor 5!</p>"
-            onReady={(editor: any) => {
-                // You can store the "editor" and use when it is needed.
-                console.log('Editor is ready to use!', editor);
-            }}
-            onChange={(event: any, editor: any) => {
-                const data = editor.getData();
-                console.log({ event, editor, data });
-            }}
-            onBlur={(event: any, editor: any) => {
-                console.log('Blur.', editor);
-            }}
-            onFocus={(event: any, editor: any) => {
-                console.log('Focus.', editor);
-            }}
-        />
-    </div>
+  return <div style={{ width: '100%', minHeight: '100vh' }}>
+    <CKEditor
+      editor={ClassicEditor}
+      data="<p></p>"
+      onReady={(editor: any) => {
+        // You can store the "editor" and use when it is needed.
+        console.log('Editor is ready to use!', editor);
+      }}
+      onChange={(event: any, editor: any) => {
+        const data = editor.getData();
+        console.log({ event, editor, data });
+      }}
+      onBlur={(event: any, editor: any) => {
+        console.log('Blur.', editor);
+      }}
+      onFocus={(event: any, editor: any) => {
+        console.log('Focus.', editor);
+      }}
+    />
+  </div>
 }
 
 export default RichTextEditor;
--- src/utils/declare.global.d.ts
+++ src/utils/declare.global.d.ts
@@ -1,5 +1,6 @@
 declare module '@ckeditor/ckeditor5-react' {
     export const CKEditor: Function;
+    export const CKEditorContext: Function;
 }
 declare module '@ckeditor/ckeditor5-build-classic' {
     export default ClassicEditor;
@@ -10,3 +11,11 @@ declare module '@ckeditor/ckeditor5-image/src/imagecaption'
 declare module '@ckeditor/ckeditor5-image/src/imagestyle'
 declare module '@ckeditor/ckeditor5-image/src/imagetoolbar'
 declare module '@ckeditor/ckeditor5-image/src/imageupload'
+declare module '@ckeditor/ckeditor5-basic-styles/src/bold'
+declare module '@ckeditor/ckeditor5-essentials/src/essentials';
+declare module '@ckeditor/ckeditor5-paragraph/src/paragraph';
+declare module '@ckeditor/ckeditor5-basic-styles/src/italic';
+declare module '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
+declare module '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+declare module '@ckeditor/ckeditor5-alignment/src/alignment';
+declare module '@ckeditor/ckeditor5-core/src/context';
