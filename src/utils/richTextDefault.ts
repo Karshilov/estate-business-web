24abc7e7a3104a3c52e1f99d1358a0d87478a77b
Karshilov <karshilov@foxmail.com>
2021-07-23 16:37:32 +0800

    update

---
+++ src/utils/richTextDefault.ts
@@ -0,0 +1,2 @@
+export const defaultData = 
+    `<h2>一级标题</h2><h3>二级标题</h3><h4>三级标题</h4><p>正文</p><blockquote><p>引用</p></blockquote><ul><li>列表项1</li><li>列表项2</li></ul><ol><li>带数字的列表项1</li><li>带数字的列表项2</li></ol><p><a href="www.baidu.com">百度</a></p><figure class="table"><table><tbody><tr><td>这</td><td>是</td><td>个</td></tr><tr><td>3</td><td>x</td><td>3</td></tr><tr><td>的</td><td>表</td><td>格</td></tr></tbody></table></figure>`;
