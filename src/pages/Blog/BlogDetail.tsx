ddda01c6550a457606d688ede60089427d7babd1
Karshilov <karshilov@foxmail.com>
2021-07-25 19:02:44 +0800

    rich avatar

--- src/pages/Blog/BlogDetail.tsx
+++ src/pages/Blog/BlogDetail.tsx
@@ -3,7 +3,8 @@ import moment from 'moment';
 import { Container } from "../../components/BasicHTMLElement";
 
 const BlogDetail = (props: { match: any }) => {
-    return <Container>
+    return <Container style={{ width: '70%', marginTop: 50, marginLeft: '15%', marginRight: '15%' }} bodyStyle={{ width: '100%', paddingBlock: 35 }}>
+        
     </Container>
 }
 
