f15eceffc950ff863318562d7ba25865d4565595
Karshilov <karshilov@foxmail.com>
2021-07-27 23:38:18 +0800

    update baseURL

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,7 +4,7 @@ import { StoreState } from '../store'
 
 // 无需token
 export const staticApi = axios.create({
-  baseURL: 'http://localhost:3383/',
+  baseURL: 'http://api.karshilov.com/',
   transformResponse(data: any) {
     // 对 data 进行任意转换处理
     const parsedData = JSON.parse(data)
@@ -17,7 +17,7 @@ export const staticApi = axios.create({
 export const useApi = () => {
   const { apiToken } = useSelector((state: StoreState) => state, (left: StoreState, right: StoreState) => left.apiToken === right.apiToken)
   return axios.create({
-    baseURL: 'http://localhost:3383/',
+    baseURL: 'http://api.karshilov.com/',
     headers: {
       'x-api-token': apiToken
     },
