6708a6d95c7d544b6f424000e3ab3f4016e0d2d7
Karshilov <karshilov@foxmail.com>
2021-07-27 23:17:46 +0800

    update auth guard

--- src/components/AuthGuard.tsx
+++ src/components/AuthGuard.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import Login from '../pages/Login';
 import { StoreState } from "../store";
+import { useApi } from "../utils/api";
 
 type Props = {
   history?: any;
@@ -11,11 +12,25 @@ type Props = {
 
 const AuthGuard: React.FC<Props> = ({ children, history }: Props) => {
   const dispatch = useDispatch();
+  const api = useApi();
 
 // 获取登录状态
   const { isLogin } = useSelector((state: StoreState) => state);
 
+  const getResult = async () => {
+    const res = await api.get('/test');
+    return res.data.success;
+  }
+
 // token过期检验待添加
+  useEffect(() => {
+    getResult().then((value) => {
+      if (value) return;
+      else {
+        dispatch({ type: 'logout' });
+      }
+    })
+  }, [])
 
   return <>{isLogin ? children : <Login isPrepared={true} />}</>;
 };
